fde27f4698252d02f8679a977a199f3b14e65773
prasadsource <62020410+prasadsource@users.noreply.github.com>
2021-01-21 12:15:41 +0530

    Update Renderer.java
    added by other user to update method1

--- src/main/java/com/learn/Renderer.java
+++ src/main/java/com/learn/Renderer.java
@@ -15,6 +15,6 @@ public class Renderer {
     }
 
     public void method1(){
-        System.out.println("New line is added to add feature154");
+        System.out.println("New line is added to add code merged by other branch");
     }
 }
